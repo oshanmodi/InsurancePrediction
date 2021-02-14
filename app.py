52854182d4e7fece8198f826c9e8e21f0a66b19c
Oshan Modi <modi.oshan@gmail.com>
2021-02-14 20:35:41 +0530

    Oshan| Working deployement, Pushing to host on Heroku.

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, url_for, redirect, render_template, jsonify
+from pycaret.regression import *
+import pandas as pd
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model = load_model('deployment_28042020')
+
+cols = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = [x for x in request.form.values()]
+    final = np.array(int_features)
+    data_unseen = pd.DataFrame([final], columns=cols)
+
+    prediction = predict_model(model, data=data_unseen, round=0)
+
+    prediction = int(prediction.Label[0])
+
+    return render_template('home.html', pred=f'Expected Bill will be {prediction}')
+
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    pass
+
+
+if __name__ == 'main':
+    app.run(debug=True)
